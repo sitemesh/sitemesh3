7acb21a1c478d064f2db8ab99141c9d805a4277e
Joe Walnes <joe@walnes.com>
2009-09-05 12:02:56 -0500

    Added support to automatically reload XML config file if it changes at runtime without having to restart web-app. Also added some hooks to make it easier for users to add custom programatic config.

--- sitemesh/src/main/java/org/sitemesh/config/ConfigurableSiteMeshFilter.java
+++ sitemesh/src/main/java/org/sitemesh/config/ConfigurableSiteMeshFilter.java
@@ -14,17 +14,19 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Enumeration;
+import java.util.logging.Logger;
 import java.io.IOException;
 import java.io.File;
 import java.io.InputStream;
 
 /**
- * A SiteMesh filter that can be dropped in to web.xml, and configured with init-params,
- * without the user having to write any Java code or use additional configuration files.
+ * A SiteMesh filter that can be dropped in to web.xml, that does not require having to write any Java code.
+ *
+ * <h2>Approach 1: Embed configuration as init-params</h2>
  *
  * <p>The minimum required to make this useful is to add a {@code decoratorMappings} init parameter.</p>
  *
- * <h3>Example (web.xml)</h3>
+ * <h3>/WEB-INF/web.xml</h3>
  * <pre>
  *  &lt;filter&gt;
  *    &lt;filter-name&gt;sitemesh&lt;/filter-name&gt;
@@ -44,7 +46,37 @@ import java.io.InputStream;
  *  &lt;/filter-mapping&gt;
  * </pre>
  *
- * <h3>Reference</h3>
+ * <h2>Approach 2: Embed configuration as init-params</h2>
+ *
+ * <p>Alternatively the configuration can be specified in a separate XML file. This has the advantage
+ * that it can be changed at runtime without having to restart the web-application. Also, the same
+ * config file could potentially be reused to generate offline content.</p>
+ *
+ * <h3>/WEB-INF/web.xml</h3>
+ * <pre>
+ *  &lt;filter&gt;
+ *    &lt;filter-name&gt;sitemesh&lt;/filter-name&gt;
+ *    &lt;filter-class&gt;org.sitemesh.config.ConfigurableSiteMeshFilter&lt;/filter-class&gt;
+ *  &lt;/filter&gt;
+ *
+ *  &lt;filter-mapping&gt;
+ *    &lt;filter-name&gt;sitemesh&lt;/filter-name&gt;
+ *    &lt;url-pattern&gt;/*&lt;/url-pattern&gt;
+ *  &lt;/filter-mapping&gt;
+ * </pre>
+ *
+ * <h3>/WEB-INF/sitemesh3.xml</h3>
+ * <pre>
+ *  &lt;sitemesh&gt;
+ *    &lt;mapping name="/*" decorator="/decorators/my-decorator.html"/&gt;
+ *    &lt;mapping name="/admin/*" decorator="/decorators/admin-decorator.html"/&gt;
+ *  &lt;/sitemesh&gt;
+ * </pre>
+ *
+ * <p>The default configuration path of <code>/WEB-INF/sitemesh3.xml</code> can be changed by
+ * adding a <code>configFile</code> init-param to the filter.</p>
+ *
+ * <h1>Reference</h1>
  *
  * <p><b><code>decoratorMappings</code></b>: A list of mappings of path patterns to decorators.
  * Each entry should consist of pattern=decorator, separated by whitespace or commas. If multiple decorators
@@ -79,25 +111,58 @@ import java.io.InputStream;
  */
 public class ConfigurableSiteMeshFilter implements Filter {
 
-    private Filter filter;
+    // TODO: This class could needs thorough unit-tests.
 
-    public synchronized void init(FilterConfig filterConfig) throws ServletException {
-        filter = setup(filterConfig, getConfigProperties(filterConfig));
-        if (filter == null) {
-            throw new ServletException(getClass().getName() + ".setup(FilterConfig) returned null");
+    private final static Logger logger = Logger.getLogger(ConfigurableSiteMeshFilter.class.getName());
+
+    // Final(ish) fields. These are intialized during init() and will never change. Can't be marked final
+    // as they can't be initialized in constructor.
+    private boolean initialized;
+    private File xmlConfigFile;
+    private FilterConfig filterConfig;
+    private Map<String,String> configProperties;
+    private boolean autoReload;
+
+    // Fields that may change during the lifecycle of the Filter (if enableReload==true).
+    // Modifications of these should be synchronized on configLock (below).
+    private volatile Filter filter;
+    private volatile long timestampOfXmlFileAtLastLoad;
+
+    // See above.
+    private final Object configLock = new Object();
+
+    public static final String CONFIG_FILE_PARAM = "configFile";
+    public static final String CONFIG_FILE_DEFAULT = "/WEB-INF/sitemesh3.xml";
+
+    public static final String AUTO_RELOAD_PARAM = "autoReload";
+    public static final boolean AUTO_RELOAD_DEFAULT = true;
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+        configProperties = getConfigProperties(filterConfig);
+        autoReload = getAutoReload();
+        logger.config("Auto reloading " + (autoReload ? "enabled" : "disabled"));
+
+        synchronized (configLock) {
+            deployNewFilter(setup());
         }
-        filter.init(filterConfig);
+        initialized = true;
     }
 
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        if (filter == null) {
+        if (!initialized) {
             throw new ServletException(getClass().getName() + ".init(FilterConfig) was not called");
         }
+        reloadIfNecessary();
         filter.doFilter(servletRequest, servletResponse, filterChain);
     }
 
-    public synchronized void destroy() {
-        filter.destroy();
+    public void destroy() {
+        synchronized (configLock) {
+            if (filter != null) {
+                filter.destroy();
+            }
+        }
     }
 
     /**
@@ -116,19 +181,47 @@ public class ConfigurableSiteMeshFilter implements Filter {
         return initParams;
     }
 
-    @SuppressWarnings("UnusedDeclaration")
-    protected Filter setup(FilterConfig filterConfig, Map<String, String> initParams) throws ServletException {
+    protected Filter setup() throws ServletException {
         ObjectFactory objectFactory = getObjectFactory();
         SiteMeshFilterBuilder builder = new SiteMeshFilterBuilder();
 
-        new PropertiesFilterConfigurator(objectFactory, initParams)
+        new PropertiesFilterConfigurator(objectFactory, configProperties)
                 .configureFilter(builder);
 
         new XmlFilterConfigurator(getObjectFactory(),
-                loadConfigXml(filterConfig, getConfigFileName(initParams)))
+                loadConfigXml(filterConfig, getConfigFileName()))
                 .configureFilter(builder);
 
+        applyCustomConfiguration(builder);
+
         return builder.create();
+    }
+
+    /**
+     * Override this to apply custom configuration after after the default configuration mechanisms.
+     */
+    @SuppressWarnings("UnusedDeclaration")
+    protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
+    }
+
+    /**
+     * Determine if a reload is required. Override this to change the behavior.
+     */
+    protected boolean reloadRequired() {
+        return timestampOfXmlFileAtLastLoad != xmlConfigFile.lastModified();
+    }
+
+    /**
+     * Whether the config file should be monitored for changes and automatically reloaded.
+     */
+    protected boolean getAutoReload() {
+        String autoReload = configProperties.get(AUTO_RELOAD_PARAM);
+        if (autoReload == null) {
+            return AUTO_RELOAD_DEFAULT;
+        } else {
+            String lower = autoReload.toLowerCase();
+            return lower.equals("1") || lower.equals("true") || lower.equals("yes");
+        }
     }
 
     /**
@@ -136,10 +229,10 @@ public class ConfigurableSiteMeshFilter implements Filter {
      * Looks for a 'config' property in the Filter init-params. 
      * If not found, defaults to '/WEB-INF/sitemesh3.xml'.
      */
-    protected String getConfigFileName(Map<String, String> initParams) {
-        String config = initParams.get("config");
+    protected String getConfigFileName() {
+        String config = configProperties.get(CONFIG_FILE_PARAM);
         if (config == null || config.isEmpty()) {
-            config = "/WEB-INF/sitemesh3.xml";
+            config = CONFIG_FILE_DEFAULT;
         }
         return config;
     }
@@ -158,32 +251,36 @@ public class ConfigurableSiteMeshFilter implements Filter {
      * </pre>
      */
     protected Element loadConfigXml(FilterConfig filterConfig, String configFilePath) throws ServletException {
-        // TODO: Don't do this every request!
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = factory.newDocumentBuilder();
 
-            File configFile = new File(configFilePath);
+            xmlConfigFile = new File(configFilePath);
 
             ServletContext servletContext = filterConfig.getServletContext();
 
             if (servletContext.getRealPath(configFilePath) != null) {
-                configFile = new File(servletContext.getRealPath(configFilePath));
+                xmlConfigFile = new File(servletContext.getRealPath(configFilePath));
             }
 
-            if (configFile.canRead()) {
+            if (xmlConfigFile.canRead()) {
                 try {
-                    Document document = documentBuilder.parse(configFile);
+                    timestampOfXmlFileAtLastLoad = xmlConfigFile.lastModified();
+                    logger.config("Loading SiteMesh 3 config file: " + xmlConfigFile.getAbsolutePath());
+                    Document document = documentBuilder.parse(xmlConfigFile);
                     return document.getDocumentElement();
                 } catch (SAXException e) {
-                    throw new ServletException("Could not parse " + configFile.getAbsolutePath(), e);
+                    throw new ServletException("Could not parse " + xmlConfigFile.getAbsolutePath(), e);
                 }
             } else {
                 InputStream stream = servletContext.getResourceAsStream(configFilePath);
                 if (stream == null) {
+                    logger.config("No config file present - using defaults and init-params. Tried: "
+                            + xmlConfigFile.getAbsolutePath() + " and ServletContext:" + configFilePath);
                     return null;
                 }
                 try {
+                    logger.config("Loading SiteMesh 3 config file from ServletContext " + configFilePath);
                     Document document = documentBuilder.parse(stream);
                     return document.getDocumentElement();
                 } catch (SAXException e) {
@@ -199,5 +296,32 @@ public class ConfigurableSiteMeshFilter implements Filter {
             throw new ServletException("Could not initialize DOM parser", e);
         }
     }
-}
 
+    protected void reloadIfNecessary() throws ServletException {
+        // TODO: Allow finer grained control of reload strategies:
+        // - don't check file timestamp on every single request (once per N seconds).
+        // - periodically check in background, instead of blocking request threads.
+
+        if (autoReload && reloadRequired()) {
+            synchronized (configLock) { // Double check lock for performance (works in JDK5+, with volatile items).
+                if (reloadRequired()) {
+                    deployNewFilter(setup());
+                }
+            }
+        }
+    }
+
+    protected void deployNewFilter(Filter newFilter) throws ServletException {
+        Filter oldFilter = filter;
+        if (newFilter == null) {
+            throw new ServletException("Cannot deploy null filter");
+        }
+        newFilter.init(filterConfig);
+        filter = newFilter;
+        if (oldFilter != null) {
+            oldFilter.destroy();
+        }
+    }
+
+}
+    
