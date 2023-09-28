da4f502c024b4c9ca26c217416c7baaf1785b5a4
Scott Murphy <scott@alwaysvip.com>
2023-09-28 09:43:26 -0700

    Spring Boot - Default decorator support. Expose decoratorSelector, contentProcessor, and siteMeshFilter as Spring Beans

--- spring-boot-starter-sitemesh/src/main/java/org/sitemesh/autoconfigure/SiteMeshAutoConfiguration.java
+++ spring-boot-starter-sitemesh/src/main/java/org/sitemesh/autoconfigure/SiteMeshAutoConfiguration.java
@@ -1,14 +1,14 @@
 package org.sitemesh.autoconfigure;
 
-import org.sitemesh.DecoratorSelector;
 import org.sitemesh.builder.SiteMeshFilterBuilder;
 import org.sitemesh.config.ConfigurableSiteMeshFilter;
 import org.sitemesh.config.MetaTagBasedDecoratorSelector;
-import org.sitemesh.config.PathBasedDecoratorSelector;
 import org.sitemesh.config.RequestAttributeDecoratorSelector;
+import org.sitemesh.content.ContentProcessor;
 import org.sitemesh.content.tagrules.html.Sm2TagRuleBundle;
 import org.sitemesh.webapp.WebAppContext;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.filter.OrderedFilter;
@@ -41,25 +41,40 @@ public class SiteMeshAutoConfiguration {
     private List<String> bundles;
     @Value("${sitemesh.decorator.attribute:}")
     private String attribute;
+    @Value("${sitemesh.decorator.default:}")
+    private String defaultPath;
 
     @Bean
-    public FilterRegistrationBean<ConfigurableSiteMeshFilter> sitemesh3(){
-        FilterRegistrationBean<ConfigurableSiteMeshFilter> registrationBean
-                = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new ConfigurableSiteMeshFilter() {
+    @ConditionalOnMissingBean(name = "decoratorSelector")
+    MetaTagBasedDecoratorSelector decoratorSelector() {
+        MetaTagBasedDecoratorSelector decoratorSelector;
+        if (attribute != null) {
+            decoratorSelector = new RequestAttributeDecoratorSelector()
+                    .setDecoratorAttribute(attribute);
+        } else {
+            decoratorSelector = new MetaTagBasedDecoratorSelector<WebAppContext>();
+        }
+        if (defaultPath != null) {
+            decoratorSelector.put("/*", defaultPath);
+        }
+        decoratorSelector.setMetaTagName(metaTagName).setPrefix(prefix);
+        return decoratorSelector;
+    }
+
+    private SiteMeshFilterBuilder siteMeshFilterBuilder;
+
+    @Bean
+    ContentProcessor contentProcessor(ConfigurableSiteMeshFilter siteMeshFilter) {
+        return siteMeshFilterBuilder.getContentProcessor();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "siteMeshFilter")
+    ConfigurableSiteMeshFilter siteMeshFilter(MetaTagBasedDecoratorSelector decoratorSelector) {
+        return new ConfigurableSiteMeshFilter() {
             @Override
             protected void applyCustomConfiguration(SiteMeshFilterBuilder builder) {
-                MetaTagBasedDecoratorSelector decoratorSelector;
-                if (attribute != null) {
-                    decoratorSelector = new RequestAttributeDecoratorSelector()
-                        .setDecoratorAttribute(attribute);
-                } else {
-                    decoratorSelector = new MetaTagBasedDecoratorSelector<WebAppContext>();
-                }
-                builder.setCustomDecoratorSelector(decoratorSelector
-                    .setMetaTagName(metaTagName)
-                    .setPrefix(prefix)
-                );
+                builder.setCustomDecoratorSelector(decoratorSelector);
                 if (mappings != null) {
                     for (Map<String, String> decorator : mappings) {
                         builder.addDecoratorPath(decorator.get("path"), decorator.get("decorator"));
@@ -76,8 +91,17 @@ public class SiteMeshAutoConfiguration {
                     }
                 }
                 builder.setIncludeErrorPages(includeErrorPages);
+                siteMeshFilterBuilder = builder;
             }
-        });
+        };
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(name = "sitemesh3")
+    public FilterRegistrationBean<ConfigurableSiteMeshFilter> sitemesh3(ConfigurableSiteMeshFilter siteMeshFilter){
+        FilterRegistrationBean<ConfigurableSiteMeshFilter> registrationBean
+                = new FilterRegistrationBean<>();
+        registrationBean.setFilter(siteMeshFilter);
         registrationBean.addUrlPatterns("/*");
         if (includeErrorPages) {
             registrationBean.setDispatcherTypes(EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR));
