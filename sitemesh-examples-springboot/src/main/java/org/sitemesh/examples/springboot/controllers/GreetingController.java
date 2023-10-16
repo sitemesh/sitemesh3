f68b087fd36928abe48bfc7595d946986c9e0917
Scott Murphy <scott@alwaysvip.com>
2023-10-15 21:21:43 -0700

    Demonstrate that SiteMesh does not decorate json

--- sitemesh-examples-springboot/src/main/java/org/sitemesh/examples/springboot/controllers/GreetingController.java
+++ sitemesh-examples-springboot/src/main/java/org/sitemesh/examples/springboot/controllers/GreetingController.java
@@ -1,11 +1,13 @@
 package org.sitemesh.examples.springboot.controllers;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -35,5 +37,15 @@ public class GreetingController {
     public View greetingError(@RequestParam(name="name", required=false, defaultValue="World") String name,
                             Model model, HttpServletRequest request) throws Exception {
         throw new RuntimeException("Whoops");
+    }
+
+    @GetMapping("/greeting/json")
+    public @ResponseBody String greetingJson(@RequestParam(name="pjax", required=false) Boolean pjax,
+                                             Model model, HttpServletResponse response) {
+        if (pjax == null || pjax.equals(true)) {
+            response.setContentType("text/json");
+        }
+        // demonstrates SiteMesh does not decorate json by default.
+        return String.format("{ decorated: %s}", pjax != null && pjax.equals(false));
     }
 }
