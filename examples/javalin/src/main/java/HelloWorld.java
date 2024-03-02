1a2fdcf3b0caa31040578dcffbab109b1fbca6f1
Scott Murphy Heiberg <scott@alwaysvip.com>
2024-03-01 21:25:22 -0600

    Javalin example

---
+++ examples/javalin/src/main/java/HelloWorld.java
@@ -0,0 +1,46 @@
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import jakarta.servlet.DispatcherType;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.sitemesh.config.ConfigurableSiteMeshFilter;
+import io.javalin.rendering.template.JavalinFreemarker;
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import freemarker.template.TemplateExceptionHandler;
+
+import java.util.Collections;
+import java.util.EnumSet;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        JavalinFreemarker.init(configureFreemarker("/templates"));
+
+        var app = Javalin.create(config -> {
+            config.staticFiles.add(staticFiles -> {
+                staticFiles.hostedPath = "/static";
+                staticFiles.directory = "/";
+                staticFiles.location = Location.CLASSPATH;
+            });
+            config.jetty.contextHandlerConfig(sch -> {
+                sch.addFilter(new FilterHolder(ConfigurableSiteMeshFilter.create(b ->
+                    b.setMimeTypes("text/html", "text/plain")
+                        .setDecoratorPrefix("")
+                        .addDecoratorPath("/*", "/decorators/default")
+                        .addExcludedPath("/static/*")
+                        .addExcludedPath("/decorators/*")
+                )), "/*", EnumSet.of(DispatcherType.REQUEST));
+            });
+        })
+        .get("/", ctx -> ctx.render("/hello.ftl", Collections.singletonMap("user", "Scott")))
+        .get("/decorators/default", ctx -> ctx.render("/decorators/default.ftl"))
+        .start(7070);
+    }
+
+    public static Configuration configureFreemarker(String templatePath) {
+        Configuration conf = new Configuration(Configuration.VERSION_2_3_23);
+        conf.setTemplateLoader(new ClassTemplateLoader(HelloWorld.class, templatePath));
+        conf.setDefaultEncoding("UTF-8");
+        conf.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        return conf;
+    }
+}
---
+++ examples/javalin/src/main/resources/templates/hello.ftl
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><sitemesh:write property="title"/></title>
+</head>
+<body>
+    <section class="section">
+        <div class="container">
+            <h1 class="title">Welcome</h1>
+            <p class="subtitle">
+                My first website with <strong>Bulma</strong>!
+            </p>
+        </div>
+    </section>
+    <section class="section">
+        Hello ${user} from Freemarker!
+    </section>
+</body>
+</html>
+
+
